5071e93cd4eab4ac609a030ae737a8727829caea
banuridwan <ujangdotnet@gmail.com>
2023-05-30 12:13:49 +0700

    nambah file kuis

---
+++ Kuis1/Kuis1/Program.cs
@@ -0,0 +1,19 @@
+﻿//Logic Kuis
+
+luasKeliling();
+
+static void luasKeliling()
+{
+    double keliling, luas;
+    //double phi = 3.14;
+    Console.WriteLine("--Luas dan keliling Lingkaran");
+    Console.WriteLine();
+    Console.Write("Masukan nilai jari-jari (r) : ");
+    int r = int.Parse(Console.ReadLine());
+
+    keliling = 2 * Math.PI * r;
+    luas = Math.PI * r * r;
+
+    Console.WriteLine($"Jumlah keliling: {keliling}");
+    Console.WriteLine($"Jumlah luas: {luas}");
+}
