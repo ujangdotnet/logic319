fb6fe76457505ae651bffe89be680b915f679a55
ujangdotnet <ujangdotnet@gmail.com>
2023-06-05 17:13:15 +0700

    edit logic 03, Logic 4 & Logic 5

--- Logic04/kuis04/kuis04/Program.cs
+++ Logic04/kuis04/kuis04/Program.cs
@@ -1,6 +1,16 @@
 ﻿//Kuis 04
 
-SoalEnam();
+try
+{
+    SoalEmpat();
+
+} catch (Exception e)
+{
+    Console.WriteLine("-----------------");
+    Console.WriteLine(e.Message);
+    Console.WriteLine("-----------------");
+}
+
 
 
 //Nomer 1
@@ -9,45 +19,164 @@ static void SoalSatu()
     Console.WriteLine("--Soal Nomer 1--");
     Console.WriteLine();
 
+    //INPUT
+    Console.Write("Golongan\t: ");
+    int golongan = int.Parse(Console.ReadLine());
+    Console.Write("Jam kerja\t: ");
+    int jamKerja = int.Parse(Console.ReadLine());
+
+    //PROSES
+    int upah = 0;
+    var upahLembur = 0;
+
+        //Algorithm completion
+
+        switch (golongan)
+        {
+        //Golongan 1
+        case 1:
+            if (jamKerja > 40)
+            {
+                upah = 2000 * 40;
+                upahLembur = 2000 * 3 / 2;
+            }
+            else
+            {
+                upah = 2000 * jamKerja;
+            }
+            break;
+
+        //Golongan 2
+        case 2:
+            if (jamKerja > 40)
+            {
+                upah = 3000 * 40;
+                upahLembur = 3000 * 3 / 2;
+            }
+            else
+            {
+                upah = 3000 * jamKerja;
+            }
+            break;
+
+        //Golongan 3
+        case 3:
+            if (jamKerja > 40)
+            {
+                upah = 4000 * 40;
+                upahLembur = 4000 * 3 / 2;
+            }
+            else
+            {
+                upah = 4000 * jamKerja;
+            }
+            break;
+
+        //Golongan 4
+        case 4:
+            if (jamKerja > 40)
+            {
+                upah = 5000 * 40;
+                upahLembur = 5000 * 3 / 2;
+            }
+            else
+            {
+                upah = 5000 * jamKerja;
+            }
+            break;
+
+        //Tidak termasuk golongan
+        default:
+            Console.WriteLine("Golonganmu tidak tersedia");
+            break;
+        }
+
+        //rumus
+        upahLembur *= (jamKerja %= 40);
+        int totalUpah = upah + upahLembur;
+
+    //OUTPUT
+    Console.WriteLine("---------------");
+    Console.WriteLine($"Upah \t: {upah}");
+    Console.WriteLine($"Lembur \t: {upahLembur}");
+    Console.WriteLine($"Total \t: {totalUpah}");
+
 }
 
 //Nomer 2
 static void SoalDua()
 {
-    Console.WriteLine("--Soal Nomer 1--");
+    Console.WriteLine("--Soal Nomer 2--");
     Console.WriteLine();
-    Console.Write("Masukan kalimat\t:");
+
+    //INPUT
+    Console.Write("Masukan kalimat\t: ");
     string kalimat = Console.ReadLine();
 
+    //PROSES
     string[] subs = kalimat.Split(' ');
 
-    for ( var i = 0; i < subs.Length; i++)
+    for (var i = 0; i < subs.Length; i++)
     {
-        Console.WriteLine($"Kata {i+1}: {subs[i]}");
+        Console.WriteLine($"Kata {i + 1}: {subs[i]}");
     }
+
+    //OUTPUT
+    Console.WriteLine();
+    Console.WriteLine($"Total kata adalah {subs.Length}");
 
 }
 
 //Nomer 3
 static void SoalTiga()
 {
-    Console.WriteLine("--Soal Nomer 1--");
+    Console.WriteLine("--Soal Nomer 3--");
     Console.WriteLine();
+
+    //INPUT
+    Console.Write("Masukan kalimat: ");
+    string kalimat = Console.ReadLine();
+
+    //PROSES (Cara 1)
+    kalimat = kalimat.Replace("a", "*").Replace("k", "*").Replace("y", "*").Replace("n", "*").Replace("m", "*");
+
+    //PROSES (Cara 2)
+
+    //OUTPUT
+    Console.WriteLine(kalimat);
 
 }
 
 //Nomer 4
 static void SoalEmpat()
 {
-    Console.WriteLine("--Soal Nomer 1--");
+    Console.WriteLine("--Soal Nomer 4--");
     Console.WriteLine();
+    //INPUT
+    Console.Write("Masukan kalimat: ");
+    string kalimat = Console.ReadLine();
 
+    //PROSES (Cara 1)
+    //kalimat = kalimat.Replace("a", "*").Replace("k", "*").Replace("y", "*").Replace("n", "*").Replace("m", "*");
+
+    //PROSES (Cara 2)
+    string[] array = kalimat.Split(" ");
+
+    foreach (string x in array)
+    {
+        Console.Write($"{x} ");
+
+    }
+
+    //OUTPUT
+    //Console.WriteLine(kalimat);
 }
+
 
 //Nomer 5
 static void SoalLima()
 {
-    Console.WriteLine("--Soal Nomer 1--");
+    Console.WriteLine("--Soal Nomer 5--");
     Console.WriteLine();
 
 }
