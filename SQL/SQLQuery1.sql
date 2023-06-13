830d17f38319211fdd0e1b4afb67e117282e8a95
ujangdotnet <ujangdotnet@gmail.com>
2023-06-13 10:36:37 +0700

    add file day 7,8,9

---
+++ SQL/SQLQuery1.sql
@@ -0,0 +1,90 @@
+--SQL Day 01--
+
+--//CREATE//
+--create database
+CREATE DATABASE db_kampus;
+
+--create table
+CREATE TABLE mahasiswa(
+id BIGINT PRIMARY KEY IDENTITY(1,1),
+name VARCHAR(50) NOT NULL,
+address VARCHAR(50) NOT NULL,
+email VARCHAR(255) NULL
+);
+
+CREATE TABLE biodata(
+id BIGINT PRIMARY KEY IDENTITY(1,1),
+mahasiswa_id BIGINT,
+tgl_lahir DATETIME,
+gender VARCHAR(10)
+);
+
+--create view
+CREATE VIEW vw_mahasiswa as SELECT * FROM mahasiswa;
+
+--//ALTER//
+--add column
+ALTER TABLE mahasiswa add nomor_hp VARCHAR(100) NOT NULL;
+
+--drop column
+ALTER TABLE mahasiswa drop column nomor_hp;
+
+--alter column
+--ALTER TABLE mahasiswa
+
+--//DROP
+--drop database
+DROP DATABASE [nama-database];
+
+
+
+--DML (Data Manipulation Language)
+
+--Insert into
+INSERT INTO mahasiswa(name,address,email) VALUES ('Banu','Ciamis','banu@gmail.com');
+
+INSERT INTO biodata(mahasiswa_id,tgl_lahir,gender) VALUES (1,'2003-06-12','laki-laki');
+INSERT INTO biodata(mahasiswa_id,tgl_lahir,gender) VALUES (2,'2005-06-12','laki-laki');
+INSERT INTO biodata(mahasiswa_id,tgl_lahir,gender) VALUES (2,'2007-06-12','wanita');
+
+--Select
+SELECT id,name,address,email FROM mahasiswa;
+
+SELECT * FROM biodata;
+
+--Update
+UPDATE mahasiswa SET name = 'Banu 2' WHERE id = 1;
+
+UPDATE biodata
+SET mahasiswa_id = 2
+
+--Delete
+DELETE FROM mahasiswa WHERE  id = 1;
+
+--Join (AND, OR, NOT)
+SELECT mhs.id as id_mahasiswa, mhs.name as nama_mahasiswa, mhs.address as alamat, mhs.email as email, bio.tgl_lahir as tgl_lahir, bio.gender as gender
+FROM mahasiswa as mhs
+JOIN biodata as bio
+ON mhs.id = bio.mahasiswa_id 
+
+--Order by
+SELECT *
+FROM biodata
+ORDER BY tgl_lahir DESC
+
+--Select Top
+SELECT TOP 1 mahasiswa_id
+FROM biodata
+ORDER BY mahasiswa_id ASC
+
+--Between
+SELECT *
+FROM biodata
+WHERE id
+BETWEEN 2 AND 3
+
+--Like
+
+--Group by
+
+--having 
---
+++ hackerrank/day1/hackerank-day1/hackerank-day1/Program.cs
@@ -0,0 +1,119 @@
+﻿
+//PlusMinus();
+//MinMaxSum();
+//CakeCandles();
+DiagonalDifference();
+
+//Plus Minus
+static void PlusMinus()
+{
+    List<int> arr = new List<int>() {-4,3,-9,0,4,1};
+    decimal positifNum = 0;
+    float nolNum = 0;
+    float negatifNum = 0;
+
+    for (int i = 0; i < arr.Count; i++)
+    {
+        if (arr[i] >= -100 && arr[i] < 0)
+        {
+            negatifNum++;
+            
+        }else if (arr[i] == 0)
+        {
+            nolNum++;
+            
+        } else if (arr[i] <= 100 && arr[i] > 0)
+        {
+            positifNum++;
+            
+        }
+    }
+
+    Console.WriteLine(Math.Round(positifNum/arr.Count, 6));
+    Console.WriteLine(nolNum/arr.Count);
+    Console.WriteLine(negatifNum/arr.Count);
+}
+
+//Mini-Max Sum
+static void MinMaxSum()
+{
+    //List<int> arr = new List<int>() {7,69,2,221,8974};
+    List<int> arr = new List<int>() {256741038, 623958417, 467905213, 714532089, 938071625};
+    arr.Sort();
+
+    long minSum = 0;
+    long maxSum = 0;
+
+    for(int i = 0; i < arr.Count; i++)
+    {
+        if (i < arr.Count - 1)
+        {
+            minSum += arr[i];
+        }
+        if (i > 0)
+        {
+            maxSum += arr[i];
+        }
+    }
+
+    Console.WriteLine(minSum);
+    Console.WriteLine(maxSum);
+}
+
+//Birthday Cake Candles
+static void CakeCandles()
+{
+    List<int> arr = new List<int>() { 99, 99, 99, 99};
+
+    int candles = 0;
+
+    for (int i = 0; i < arr.Count; i++)
+    {
+        if(arr[i] == arr.Max())
+        {
+            candles++;
+        }
+    }
+
+    Console.WriteLine(candles);
+}
+
+//Diagonal Difference
+static void DiagonalDifference()
+{
+    //Array
+    int[,] arr =
+    {
+        {1,2,3 },
+        {4,5,6},
+        {7,8,9 }
+    };
+
+    int diagonalSatu = 0;
+    int diagonalDua = 0;
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (i == j)
+            {
+                diagonalSatu += arr[i,j];
+            }
+
+            if (i == arr.GetLength(1) - 1 - i)
+            {
+                diagonalDua += arr[i, arr.GetLength(1) - 1 - i];
+            }
+
+        }
+    }
+
+    Console.WriteLine(diagonalSatu);
+    Console.WriteLine(diagonalDua);
+
+    //List
+    int[,] angka = { { 1, 2, 3 }, { 4,5, 6 }, { 7,8, 9 } };
+
+}
